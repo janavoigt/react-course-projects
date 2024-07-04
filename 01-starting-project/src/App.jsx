6b2e421f1866ff5c0f880a2fe7ac0a1b4e33a845
Janaina Voigt <jv.de@icloud.com>
2024-07-04 17:40:17 +0200

    Added the topics

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 import CoreConcept from "./components/CoreConcepts";
 import Header from "./components/Header";
 import TabButton from "./components/TabButton";
+import { EXAMPLES } from "../lib/data";
 
 function App() {
   const [selectTopic, setSelectTopic] = useState();
+
   function handleSelect(selectButton) {
     setSelectTopic(selectButton);
   }
+
   return (
     <div>
       <Header />
@@ -22,7 +25,17 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          {selectTopic}
+          {!selectTopic ? (
+            <p>Please select a Topic.</p>
+          ) : (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectTopic].title}</h3>
+              <p>{EXAMPLES[selectTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectTopic].code}</code>
+              </pre>
+            </div>
+          )}
         </section>
       </main>
     </div>
