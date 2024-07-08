fefe7abe4a30d6d59e8e8ef56d046270cded857a
Janaina Voigt <jv.de@icloud.com>
2024-07-08 14:22:47 +0200

    Setting default prop values in the Tabs component

--- 01-starting-project/src/components/Tabs.jsx
+++ 01-starting-project/src/components/Tabs.jsx
@@ -1,7 +1,7 @@
-export default function Tabs({ buttons, children }) {
+export default function Tabs({ buttons, children, ButtonContainer = "menu" }) {
   return (
     <>
-      <menu>{buttons}</menu>
+      <ButtonContainer>{buttons}</ButtonContainer>
       {children}
     </>
   );
