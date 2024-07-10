00d7f0a4bded3cefc0013bd443edf26a2cff193c
Janaina Voigt <jv.de@icloud.com>
2024-07-10 13:19:24 +0200

    Added useState with even target

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -4,8 +4,8 @@ function App() {
   return (
     <div id="game-container">
       <ol id="players">
-        <Player namePlayer="PLAYER 1" symbol="X" />
-        <Player namePlayer="PLAYER 2" symbol="O" />
+        <Player inicialName="PLAYER 1" symbol="X" />
+        <Player inicialName="PLAYER 2" symbol="O" />
       </ol>
     </div>
   );
--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,16 +1,28 @@
 import { useState } from "react";
 
-export default function Player({ namePlayer, symbol }) {
+export default function Player({ inicialName, symbol }) {
+  const [namePlayer, setNamePlayer] = useState(inicialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
-    setIsEditing(true);
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleNameChance(event) {
+    setNamePlayer(event.target.value);
   }
 
   let playerName = <span className="player-name">{namePlayer}</span>;
 
   if (isEditing) {
-    playerName = <input type="text" required />;
+    playerName = (
+      <input
+        type="text"
+        required
+        value={namePlayer}
+        onChange={handleNameChance}
+      />
+    );
   }
 
   return (
@@ -19,7 +31,7 @@ export default function Player({ namePlayer, symbol }) {
         {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
