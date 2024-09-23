2098389f0a3eb40a5cf8fd636000799e383664f7
Janaina Voigt <jv.de@icloud.com>
2024-09-23 15:46:00 +0200

    Added the new component userInput

--- 05-Investment-Calculator-App/src/components/UserInput.jsx
+++ 05-Investment-Calculator-App/src/components/UserInput.jsx
@@ -1,1 +1,70 @@
-export default function UserInput() {}
+import { useState } from "react";
+
+export default function UserInput() {
+  const [userInput, setUserInput] = useState({
+    inicialInvestment: 10000,
+    annualInvestment: 1200,
+    expectReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label htmlFor="">Inicial Investment</label>
+          <input
+            type="number"
+            required
+            value={userInput.inicialInvestment}
+            onChange={(event) =>
+              handleChange("inicialInvestment", event.target.value)
+            }
+          />
+        </p>
+        <p>
+          <label htmlFor="">Annual Investment</label>
+          <input
+            type="number"
+            required
+            value={userInput.annualInvestment}
+            onChange={(event) =>
+              handleChange("annualInvestiment", event.target.value)
+            }
+          />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label htmlFor="">Expected return</label>
+          <input
+            type="number"
+            required
+            value={userInput.expectReturn}
+            onChange={(event) =>
+              handleChange("expectedReturn", event.target.value)
+            }
+          />
+        </p>
+        <p>
+          <label htmlFor="">Duration</label>
+          <input
+            type="number"
+            required
+            value={userInput.duration}
+            onChange={(event) => handleChange("durantion", event.target.value)}
+          />
+        </p>
+      </div>
+    </section>
+  );
+}
