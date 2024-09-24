57b076ab8ae4d60c9e6ea8b364ed7e55bb01f12d
Janaina Voigt <jv.de@icloud.com>
2024-09-24 16:38:26 +0200

    Added the variable inputIsValid because the duration must to be a positiv number

--- 05-Investment-Calculator-App/src/App.jsx
+++ 05-Investment-Calculator-App/src/App.jsx
@@ -11,6 +11,8 @@ function App() {
     duration: 10,
   });
 
+  const inputIsValid = userInput.duration >= 1;
+
   function handleChange(inputIdentifier, newValue) {
     setUserInput((prevUserInput) => {
       return {
@@ -19,11 +21,13 @@ function App() {
       };
     });
   }
+
   return (
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange} />
-      <Results input={userInput} />
+      {!inputIsValid && <p className="center">Please enter a Duration.</p>}
+      {inputIsValid && <Results input={userInput} />}
     </>
   );
 }
