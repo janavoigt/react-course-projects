147a19981e2b7a1fababdebf5a83d371655bf0e2
Janaina Voigt <jv.de@icloud.com>
2024-10-10 14:30:39 +0200

    Added the useImpeativeHandle to the ResultModal component

--- 08-Refs-Portals-starting-project/src/components/TimeChallenge.jsx
+++ 08-Refs-Portals-starting-project/src/components/TimeChallenge.jsx
@@ -12,7 +12,7 @@ export default function TimeChallenge({ title, targetTime }) {
     setTimerStart(true);
     timer.current = setTimeout(() => {
       setTimerExpired(true);
-      dialog.current.showModal();
+      dialog.current.open();
     }, targetTime * 1000);
   }
 
