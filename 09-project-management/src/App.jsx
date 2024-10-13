91cf5fff5bff65b17cd080fe73d3883d58ff0d70
Janaina Voigt <jv.de@icloud.com>
2024-10-13 22:55:19 +0200

    Added the new component

--- 09-project-management/src/App.jsx
+++ 09-project-management/src/App.jsx
@@ -1,11 +1,12 @@
 import NewProject from "./components/NewProject";
+import NoProjectselected from "./components/NoProjectselected";
 import ProjectSideBar from "./components/ProjectSideBar";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectSideBar />
-      <NewProject />
+      <NoProjectselected />
     </main>
   );
 }
