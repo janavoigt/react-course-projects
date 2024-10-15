c21d797b9d4517ae8a98bfaee8e1c5e140b7d949
Janaina Voigt <jv.de@icloud.com>
2024-10-15 14:12:29 +0200

    Added the Modal componente and the useImperativehandle function

--- 09-project-management/src/App.jsx
+++ 09-project-management/src/App.jsx
@@ -28,7 +28,10 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSideBar onAddProject={handleStartAddProject} />
+      <ProjectSideBar
+        onAddProject={handleStartAddProject}
+        projects={projects}
+      />
       {content}
     </main>
   );
