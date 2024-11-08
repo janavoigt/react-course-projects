0d98d9e998fdb81c2b5769892238298ae8b2bf22
Janaina Voigt <jv.de@icloud.com>
2024-11-08 15:29:43 +0100

    Added the async function

--- 14-HTTP-Requests/src/components/AvailablePlaces.jsx
+++ 14-HTTP-Requests/src/components/AvailablePlaces.jsx
@@ -5,13 +5,16 @@ export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/places")
-      .then((response) => {
-        return response.json();
-      })
-      .then((resData) => {
+    async function fecthPlaces() {
+      try {
+        const response = await fetch("http://localhost:3000/places");
+        const resData = await response.json();
         setAvailablePlaces(resData.places);
-      });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fecthPlaces();
   }, []);
 
   return (
