0b1724af0bc08dd398b53629fb7261a4131d35b5
Janaina Voigt <jv.de@icloud.com>
2024-11-11 17:38:40 +0100

    Added the Error component

--- 14-HTTP-Requests/src/components/AvailablePlaces.jsx
+++ 14-HTTP-Requests/src/components/AvailablePlaces.jsx
@@ -1,27 +1,41 @@
 import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
+import Error from "./Error.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
     async function fecthPlaces() {
       try {
         const response = await fetch("http://localhost:3000/places");
         const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch places");
+        }
         setAvailablePlaces(resData.places);
       } catch (error) {
-        console.log(error);
+        setError({ message: error.message || "Try again later" });
       }
+      setIsFetching(false);
     }
     fecthPlaces();
   }, []);
+
+  if (error) {
+    return <Error title="An error occurred" message={error.message} />;
+  }
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
       fallbackText="No places available."
+      isLoading={isFetching}
+      loadingText="Carregando Places..."
       onSelectPlace={onSelectPlace}
     />
   );
