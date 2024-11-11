6f5b2e3b3aece9bf030416f58f38c2d5db4cb43d
Janaina Voigt <jv.de@icloud.com>
2024-11-11 18:31:22 +0100

    Added the fetch in the other file to be reused

--- 14-HTTP-Requests/src/components/AvailablePlaces.jsx
+++ 14-HTTP-Requests/src/components/AvailablePlaces.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
 import Error from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
+import { fetchAvailablePlaces } from "../http.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -10,13 +12,17 @@ export default function AvailablePlaces({ onSelectPlace }) {
   useEffect(() => {
     async function fecthPlaces() {
       try {
-        const response = await fetch("http://localhost:3000/places");
-        const resData = await response.json();
+        const places = await fetchAvailablePlaces();
 
-        if (!response.ok) {
-          throw new Error("Failed to fetch places");
-        }
-        setAvailablePlaces(resData.places);
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
       } catch (error) {
         setError({ message: error.message || "Try again later" });
       }
