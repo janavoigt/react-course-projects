25f6deff035e8c8c14c650ae9544d830a9728b7f
Janaina Voigt <jv.de@icloud.com>
2024-11-12 15:12:07 +0100

    Added the PUT request when you select a place

--- 14-HTTP-Requests/src/http.js
+++ 14-HTTP-Requests/src/http.js
@@ -7,3 +7,19 @@ export async function fetchAvailablePlaces() {
   }
   return resData.places;
 }
+
+export async function UpdateUserPlaces(places) {
+  const response = await fetch("http://localhost:3000/user-places", {
+    method: "PUT",
+    body: JSON.stringify({ places }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error("failed to update user data");
+  }
+  return resData.message;
+}
