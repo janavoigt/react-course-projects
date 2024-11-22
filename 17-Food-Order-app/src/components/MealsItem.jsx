b45a11028e3688285f5b994cfd4430df48edf1b1
Janaina Voigt <jv.de@icloud.com>
2024-11-22 17:04:42 +0100

    Created the const formatting.js file with the const for the currency price

--- 17-Food-Order-app/src/components/MealsItem.jsx
+++ 17-Food-Order-app/src/components/MealsItem.jsx
@@ -5,7 +5,9 @@ export default function MealsItem({ meal }) {
         <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
         <div>
           <h3>{meal.name}</h3>
-          <p className="meal-item-price">{meal.price}</p>
+          <p className="meal-item-price">
+            {currencyFormatter.format(meal.price)}
+          </p>
           <p className="meal-item-description">{meal.description}</p>
         </div>
         <p className="meal-item-actions">
