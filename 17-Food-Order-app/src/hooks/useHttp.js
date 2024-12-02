1c16076cea52d6f4f098a706c4f27015689cfcd5
Janaina Voigt <jv.de@icloud.com>
2024-12-02 22:24:20 +0100

    Added the clearCart function and fixed the bug with the Cart

--- 17-Food-Order-app/src/hooks/useHttp.js
+++ 17-Food-Order-app/src/hooks/useHttp.js
@@ -19,11 +19,15 @@ export default function useHttp(url, config, initialData) {
   const [isLoading, setIsLoading] = useState(false);
   const [data, setData] = useState(initialData);
 
+  function clearData() {
+    setData(initialData);
+  }
+
   const sendRequest = useCallback(
-    async function sendRequest() {
+    async function sendRequest(data) {
       setIsLoading(true);
       try {
-        const resData = await sendHttpRequest(url, config);
+        const resData = await sendHttpRequest(url, { ...config, body: data });
         setData(resData);
       } catch (error) {
         setError(
@@ -46,5 +50,6 @@ export default function useHttp(url, config, initialData) {
     error,
     isLoading,
     sendRequest,
+    clearData,
   };
 }
