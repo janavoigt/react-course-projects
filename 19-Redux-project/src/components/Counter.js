e05a14cced637738ff96e71d80aed124303a2774
Janaina Voigt <jv.de@icloud.com>
2025-05-31 22:50:46 +0200

    Started and Finished the initial project

--- 19-Redux-project/src/components/Counter.js
+++ 19-Redux-project/src/components/Counter.js
@@ -1,26 +1,34 @@
 import { useDispatch, useSelector } from 'react-redux';
 import classes from './Counter.module.css';
-
+import { counterActions } from '../store';
 
 const Counter = () => {
- const counter = useSelector(state => state.counter);
+ const counter = useSelector(state => state.counter.counter);
+ const show = useSelector(state => state.counter.showCounter);
  const dispatch = useDispatch();
 
-  const toggleCounterHandler = () => {};
-
-  const incrementHandler = () => {
-    dispatch({type: 'increment'});
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter());
   };
 
+  const incrementHandler = () => {
+    dispatch(counterActions.increment());
+  };
+
+  const increaseHandler = () => {
+    dispatch(counterActions.increase(5));
+  }
+
   const decrementHandler = () => {
-    dispatch({type: 'decrement'});
+    dispatch(counterActions.decrement());
   };
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- 19-Redux-project/src/store/index.js
+++ 19-Redux-project/src/store/index.js
@@ -1,20 +1,80 @@
-import { createStore } from 'redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
-const counterReducer = (state = {counter: 0}, action) => {
-    if(action.type === 'increment'){
-        return {
-            counter: state.counter + 1,
-        };
+const initialstate = { counter: 0, showCounter: true}
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: initialstate,
+    reducers:{
+        increment(state){
+            state.counter++;
+        },
+        decrement(state){
+            state.counter--;
+        },
+        increase(state, action){
+            state.counter = state.counter + action.payload;
+        },
+        toggleCounter(state){
+            state.showCounter = !state.showCounter;
+        }
+
     }
 
-    if(action.type === 'decrement'){
-        return{
-            counter: state.counter - 1,
-        };
-    }
-    return state;
-};
+});
 
-const store = createStore(counterReducer);
+const initialAuthState = { isAuthenticated: false};
+
+const authSlice = createSlice({
+    name: 'authentication',
+    initialState: initialAuthState,
+    reducers:{
+        login(state){
+            state.isAuthenticated = true;
+        },
+        logout(state){
+            state.isAuthenticated = false;
+        },
+    }
+
+})
+
+// const counterReducer = (state = initialstate, action) => {
+//     if(action.type === 'increment'){
+//         return {
+//             counter: state.counter + 1,
+//             showCounter: state.counter
+//         };
+//     }
+
+//     if(action.type === 'increaseby5'){
+//         return{
+//             counter: state.counter + action.amount,
+//             showCounter: state.counter 
+//         }
+//     }
+
+//     if(action.type === 'decrement'){
+//         return{
+//             counter: state.counter - 1,
+//             showCounter: state.counter
+//         };
+//     }
+
+//     if(action.type === 'toggle'){
+//         return{
+//             showCounter: !state.showCounter,
+//             counter: state.counter
+//         }
+//     }
+//     return state;
+// };
+
+const store = configureStore({
+    reducer: {counter : counterSlice.reducer, auth: authSlice.reducer}
+});
+
+export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;
